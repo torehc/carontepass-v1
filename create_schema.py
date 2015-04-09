3d03ed4466491bab74473a0848f9ca0f9db478ab
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2015-04-09 08:42:57 +0100

    Camios en local

--- create_schema.py
+++ create_schema.py
@@ -9,45 +9,10 @@ from sqlalchemy import create_engine
 
 import dba
 
-metadata = MetaData()
+
 cs = dba.get_connection_string()
-engine = create_engine(dba.get_connection_string())
-
-dba.Group.metadata.create_all(engine)
-
-metadata = dba.Group.metadata
-
-Message = Table('cp_message', metadata,
-                Column('id_message', Integer, primary_key=True),
-                Column('text', String(512)),
-                Column('user_id', Integer, ForeignKey('cp_user.id_user')),
-                Column('ts_send', DateTime()),
-                Column('ts_received', DateTime()),
-                ) 
-
-Payment = Table('cp_payment', metadata,
-	Column('id_payment', Integer, primary_key=True),
-	Column('year', Integer()),
-	Column('month', Integer()),
-	Column('user_id', Integer, ForeignKey('cp_user.id_user')),
-	Column('f_payment', DateTime()),
-	Column('amount', Numeric(10, 2)),
-        ) 
-
-Device = Table('cp_device', metadata,
-	Column('id_device', Integer, primary_key=True),
-	Column('user_id', Integer, ForeignKey('cp_user.id_user')),
-	Column('type', String(3)),
-	Column('mac', String(12)),
-	Column('nfc', String(11)),
-	)   	
-
-Log = Table('cp_log', metadata, 
-	Column('id_log', Integer, primary_key=True),
-	Column('device_id', Integer, ForeignKey('cp_device.id_device')),
-	Column('ts_input', DateTime()),
-	Column('ts_output', DateTime(), default=None),
-	)
+engine = create_engine(cs)
+dba.metadata.create_all(engine)
 
 if __name__ == '__main__':
     metadata.create_all(engine)
