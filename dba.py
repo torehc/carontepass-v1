b2d591c02740318eb7a9d2ae8204a97f044441bf
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2015-04-09 08:42:52 +0100

    Camios en local

--- dba.py
+++ dba.py
@@ -16,7 +16,8 @@ def get_connection_string():
         db=config.database,
         )
 
-Base = declarative_base()
+metadata = MetaData()
+Base = declarative_base(metadata=metadata)
 
 class Group(Base):
     __tablename__ = 'cp_group'
@@ -36,3 +37,39 @@ class User(Base):
     phone = Column(String(15))
     address = Column(String(220))
     email = Column(String(180))
+
+class Message(Base):
+    __tablename__ = 'cp_message'
+
+    id_message = Column(Integer, primary_key=True)
+    text = Column(String(512))
+    user_id = Column(Integer, ForeignKey('cp_user.id_user'))
+    ts_send = Column(DateTime)
+    ts_received = Column(DateTime)
+
+class Payment(Base):
+    __tablename__ 'cp_payment'
+
+    id_payment = Column(Integer, primary_key=True)
+    year = Column(Integer)
+    month = Column(Integer)
+    user_id = Column(Integer, ForeignKey('cp_user.id_user'))
+    f_payment = Column(DateTime)
+    amount = Column(Numeric(10, 2))
+
+class Device(Base):
+    __tablename__ 'cp_device'
+
+    id_device = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('cp_user.id_user'))
+    kind = Column(Enum(['mac', 'rfc']), nullable=False)
+    mac = Column(String(12))
+    nfc = Column(String(11))
+
+class Log(Base):
+    __tablename__ 'cp_log'
+
+    id_log = Column(Integer, primary_key=True)
+    device_id = Column(Integer, ForeignKey('cp_device.id_device'))
+    ts_input = Column(DateTime())
+    ts_output = Column(DateTime(), default=None)
