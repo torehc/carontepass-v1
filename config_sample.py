d2af292de83dd5bcb9de8599910455e0257c9c77
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2015-04-14 10:19:27 +0100

    Añadido constante MAX_GRANTED_DAYS: Máximo número de días del mes que se permite el paso si no se ha pagado el mes actual, pero se ha pagado el anterior

--- config_sample.py
+++ config_sample.py
@@ -8,3 +8,8 @@ port = 5432
 username = 'carontepass' 
 password = 'carontepass'
 database = 'carontepass'
+
+# Máximo número de días del mes que se permite el paso si no se ha pagado
+# el mes actual, pero se ha pagado el anterior
+
+MAX_GRANTED_DAYS = 15
