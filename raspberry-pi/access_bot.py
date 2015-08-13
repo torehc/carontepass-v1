cb5745bb2e16186ace988fa99fb536b797d66ebf
Torehc <torehc@gmail.com>
2015-08-13 14:12:04 +0100

    Mensaje cuando no hay usuarios

--- raspberry-pi/access_bot.py
+++ raspberry-pi/access_bot.py
@@ -22,7 +22,8 @@ def listener(messages):
 
 	for m in messages:
 		chatid = m.chat.id
-		print chatid + m.text
+		print chatid 
+		print m.text
 		#if m.content_type == 'text':
 			#print str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text
 
@@ -55,9 +56,14 @@ def open_door(m):
 def command_who(m):
 	chatid = m.chat.id
 	users = log.users_in_place()
-	msg = 'People here are: {}'.format(
-	  ', '.join([str(u.full_name()) for u in users])
-	  )
+	
+	if not users:
+		msg = 'No People'
+	else:
+		msg = 'People here are: {}'.format(
+	  	', '.join([str(u.full_name()) for u in users])
+	 	 )
+
 	bot.send_message(chatid, msg) 
 	    
 @bot.message_handler(commands=['help']) 
